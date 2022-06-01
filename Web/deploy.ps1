cf4fc91deff006aeaf28e527778f6aa52e4462b7
Scott Durret <icodeintx@gmail.com>
2022-05-31 21:09:35 -0500

    added deploy script

---
+++ Web/deploy.ps1
@@ -0,0 +1,12 @@
+
+#backup the application settings files
+Copy-Item C:\OneDrive\Personal\AmaraCode\production\apps\PaymentJournal\appsettings.* C:\OneDrive\Personal\AmaraCode\production\apps\PaymentJournal\bak
+Write-Output "Application Settings files backed up"
+
+#copies all files from the publish folder to the destination folder overwriting files as needed.
+Copy-Item -Path ".\publish\*" -Destination "C:\OneDrive\Personal\AmaraCode\production\apps\PaymentJournal\" -Force
+Write-Output "All files copied"
+
+#replace the application settings files with the originals
+Copy-Item C:\OneDrive\Personal\AmaraCode\production\apps\PaymentJournal\bak\appsettings.* C:\OneDrive\Personal\AmaraCode\production\apps\PaymentJournal\
+Write-Output "Application files replaced"
