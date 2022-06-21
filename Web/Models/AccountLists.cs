aa37c072d140cbe9197d64d3d9f0d76ad64b858a
Scott Durret <icodeintx@gmail.com>
2022-06-20 19:07:23 -0500

    starting AccountList component - created issue in github since we ran into a problem

---
+++ Web/Models/AccountLists.cs
@@ -0,0 +1,8 @@
+﻿namespace PaymentJournal.Web.Models;
+
+public class AccountLists
+{
+    public string AccountNumber { get; set; }
+    public string Description { get; set; }
+    public string Name { get; set; }
+}
---
+++ Web/Pages/Components/AccountLists.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using PaymentJournal.Web.Models;
+using PaymentJournal.Web.Repositories;
+
+namespace PaymentJournal.Web.Pages.Components;
+
+public partial class AccountLists : ComponentBase
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public AccountLists()
+    {
+    }
+
+    public List<AccountLists> Account { get; set; } = new();
+}
