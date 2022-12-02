af348881fd40de8008cafd8144f5cdc28deb845f
Scott Durrett <icodeintx@gmail.com>
2022-12-01 22:29:18 -0600

    finished MudBlazor initial implementation

--- PaymentJournal.Web/Models/PaymentItem.cs
+++ PaymentJournal.Web/Models/PaymentItem.cs
@@ -5,9 +5,13 @@ namespace PaymentJournal.Web.Models;
 public class PaymentItem
 {
     public Guid BudgetId { get; set; } = Guid.Empty;
-    public DateTime CreateDate { get; set; } = DateTime.Now;
+
+    public DateTime? CreateDate { get; set; } = DateTime.Now;
+
     public string Note { get; set; } = string.Empty;
+
     public List<Payee> Payees { get; set; } = new List<Payee>();
+
     public Guid PaymentItemId { get; set; } = Guid.Empty;
 
     public override string ToString()
