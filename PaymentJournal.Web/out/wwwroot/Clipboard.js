5c7baea194202b46d040be0f36c456fce055b707
Scott Durrett <icodeintx@gmail.com>
2025-04-02 13:09:10 -0500

    removed out folder

--- PaymentJournal.Web/out/wwwroot/Clipboard.js
+++
@@ -1,7 +0,0 @@
-window.copyToClipboard = (text) => {
-    navigator.clipboard.writeText(text).then(() => {
-        console.log("Text copied to clipboard");
-    }).catch(err => {
-        console.error("Could not copy text: ", err);
-    });
-};
